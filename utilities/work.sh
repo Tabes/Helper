3d996671b63f12f473e2a76c7cfd4ce247232398
Tabes <github@mawage.de>
2025-09-17 08:41:19 +0000

    A29910:41 AM260 299p17AM10:41 AMe

--- utilities/work.sh
+++ utilities/work.sh
@@ -5,7 +5,7 @@
 ### Provides comprehensive Configuration loading for bash Framework Projects
 ################################################################################
 ### Project: Universal Helper Library
-### Version: 2.1.10
+### Version: 2.1.11
 ### Author:  Mawage (Development Team)
 ### Date:    2025-09-17
 ### License: MIT
